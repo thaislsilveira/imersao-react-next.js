12cdcb81f0e8e1ca3adc923f24452c13fb1019d5
Thaís Silveira <thais_lsilveira@hotmail.com>
2021-01-26 09:39:43 -0300

    feat: add eslintrc

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
 import db from '../db.json';
-import Widget from '../src/components/Widget'
-import QuizLogo from '../src/components/QuizLogo'
-import QuizBackground from '../src/components/QuizBackground'
-import Footer from '../src/components/Footer'
-import GitHubCorner from '../src/components/GitHubCorner'
+import Widget from '../src/components/Widget';
+import QuizLogo from '../src/components/QuizLogo';
+import QuizBackground from '../src/components/QuizBackground';
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
 
 export const QuizContainer = styled.div`
   width: 100%;
@@ -20,27 +20,27 @@ export const QuizContainer = styled.div`
 export default function Home() {
   return (
     <QuizBackground backgroundImage={db.bg}>
-    <QuizContainer>
-      <QuizLogo />
-      <Widget>
-        <Widget.Header>
-          <h1>{db.title}</h1>
-        </Widget.Header>
-        <Widget.Content>
-          <p>{db.description}</p>
-        </Widget.Content>
-      </Widget>
+      <QuizContainer>
+        <QuizLogo />
+        <Widget>
+          <Widget.Header>
+            <h1>{db.title}</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>{db.description}</p>
+          </Widget.Content>
+        </Widget>
 
-      <Widget>
-        <Widget.Content>
-          <h1>Quizes da Galera</h1>
+        <Widget>
+          <Widget.Content>
+            <h1>Quizes da Galera</h1>
 
-          <p>lorem ipsum dolor sit amet...</p>
-        </Widget.Content>
-      </Widget>
-      <Footer />
-    </QuizContainer>
-    <GitHubCorner projectUrl="https://github.com/thaislsilveira/imersao-react-next.js" />
-  </QuizBackground>
-    )
+            <p>lorem ipsum dolor sit amet...</p>
+          </Widget.Content>
+        </Widget>
+        <Footer />
+      </QuizContainer>
+      <GitHubCorner projectUrl="https://github.com/thaislsilveira/imersao-react-next.js" />
+    </QuizBackground>
+  );
 }
